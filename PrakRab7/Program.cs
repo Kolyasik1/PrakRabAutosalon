75f9401cfc61f4e91f83bd1e6f07b97501230a27
Kolyasik1 <43873530+Kolyasik1@users.noreply.github.com>
2018-10-25 13:33:09 +0300

    Add files via upload

---
+++ PrakRab7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrakRab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, What's your name");
+            Console.WriteLine("Как я могу к Вам обращаться?");
+            string MyName = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, " + MyName
+            + "! Добро пожаловать в систему!");
+            // Задать с клавиатуры число элементов массива
+            int n;
+            Console.WriteLine("Введите число элементов массива");
+            n = Convert.ToInt16(Console.ReadLine());
+            // Объявим одномерный массив Data с n-элементами 
+            double[] Data = new double[n];
+            int i = 0;
+            while (i < n)
+            {
+                Console.WriteLine("Введите элемент массива");
+                /* Вводим элементы массива с клавиатуры 
+                 * и заполняем ими массив */
+                Data[i] = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+                i++;
+            }
+            // Вывод содержимого массива
+            for (i = 0; i < n; i = i + 1)
+                Console.WriteLine("Элемент[" + i + "]: " + Data[i]);
+            Console.ReadKey();
+        }
+    }
+}
