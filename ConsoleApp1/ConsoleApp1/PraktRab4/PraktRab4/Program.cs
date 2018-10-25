6c5968b994f45039db72ff051a8fa9a607ab401d
Kolyasik1 <43873530+Kolyasik1@users.noreply.github.com>
2018-10-25 13:19:59 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/PraktRab4/PraktRab4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraktRab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to Programm!");
+            Console.WriteLine("Please,tell me your name");
+            string MyName = Console.ReadLine();
+            Console.WriteLine("Nice to meet you, " + MyName
+                + "! I'll try to do my best!");
+            int FirstNumber, SecondNumber;
+            Console.WriteLine("Enter the FirstNumber,please");
+            FirstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the SecondNumber,please");
+            SecondNumber = int.Parse(Console.ReadLine());
+
+            do
+            {
+                FirstNumber++;
+
+            }
+            while (FirstNumber <= 1000);
+            do
+            {
+                SecondNumber++;
+            }
+            while (SecondNumber <= 1000);
+            int Step = Convert.ToInt32(FirstNumber + SecondNumber);
+            do
+            {
+                Step++;
+
+            } while (Step <= 1000);
+            Console.WriteLine(Step);
+            Console.ReadLine();
+
+        }
+    }
+}
